2d56864c21f80af8b6fd373087e85e52ae0cb65e
HassaElgallouchi <helgallouchi@gmail.com>
2021-02-02 12:15:40 +0100

    add ex 03 js

---
+++ script/ex_03.js
@@ -0,0 +1,23 @@
+let yourName = () => {
+    let nom = '';
+    let footer = document.querySelector('footer div');
+    footer.addEventListener('click', () => {
+
+        while(nom == '' || nom == null) {
+            nom = prompt('Quel est votre nom ?');
+        }
+        if(confirm(`Etes vous sûr que *${nom}* est votre nom ?`)) {
+            alert(`Bonjour *${nom}* !`)
+            footer.textContent = `Bonjour *${nom}* !`;
+        } else {
+            alert('Dommage \):')
+            footer.textContent = 'Dommage \):'
+        };
+    })
+
+
+}
+
+yourName();
+
+
